e11cddd0195d8ef94af973f03948c174edef5dd1
IvenBach <ivenbach@gmail.com>
2026-03-27 06:17:37 -0700

    Add mathematical methods

--- Adding specific parts of files/adding_specific_parts_of_a_file.py
+++ Adding specific parts of files/adding_specific_parts_of_a_file.py
@@ -15,3 +15,16 @@ def dividing_two_numbers(numerator, denominator):
 # Plenty of code in between
 
 
+# Code you're working on
+def Foo(bar, bazz):
+    match bar:
+        case 1:
+            return adding_two_numbers_together(1, 3)
+        case 2:
+            return subtracting_two_numbers(2, 2)
+        case 3:
+            return multiplying_two_numbers(3, 1)
+        case 4:
+            return dividing_two_numbers(4, 0)
+
+    return bazz
